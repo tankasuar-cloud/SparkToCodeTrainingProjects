8e2b25750080c30ede94b68dff6620ebce3908a1
Haitham Albusaidi <Tankasuar@gmail.com>
2026-06-28 20:39:00 +0400

    Add first project file

---
+++ FirstC#Project/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstC_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
