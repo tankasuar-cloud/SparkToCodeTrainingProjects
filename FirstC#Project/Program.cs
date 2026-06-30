c9345d03f204da264069c188eeb9462a71ae7372
Haitham Albusaidi <Tankasuar@gmail.com>
2026-06-30 21:17:19 +0400

    Testing today's lesson, basic input output

--- FirstC#Project/Program.cs
+++ FirstC#Project/Program.cs
@@ -4,10 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("test");
-            Console.WriteLine("test Branch");
+      
+            Console.Write("Name: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("salary: ");
+            float Salary = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("hello " + Name + " welcome to spark");
+            Console.WriteLine("You are:  " + Age + " years old");
+            Console.WriteLine("You make " + Salary + " OMR");
+            ///////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("enter first number: ");
+            float A = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number: ");
+            float B  = float.Parse(Console.ReadLine());
+
+
+            float R1 = A + B;
+            float R2 = A - B;
+            float R3 = A * B;
+            float R4 = A / B;
+            float R5 = A % B;
+
+            Console.WriteLine("Addition result: " + R1);
+            Console.WriteLine("Subtraction result: " + R2);
+            Console.WriteLine("multiplication result: " + R3);
+            Console.WriteLine("Division result: " + R4);
+            Console.WriteLine("Reminder result: " + R5);
+            
+
         }
     }
 }
