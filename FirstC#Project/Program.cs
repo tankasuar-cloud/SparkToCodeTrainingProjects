d4cfcfddaad387dea38e80ccc3a79ece5000b247
Haitham Albusaidi <Tankasuar@gmail.com>
2026-06-30 21:22:01 +0400

    Testing basic if else statment

--- FirstC#Project/Program.cs
+++ FirstC#Project/Program.cs
@@ -4,42 +4,74 @@
     {
         static void Main(string[] args)
         {
-      
-            Console.Write("Name: ");
-            string Name = Console.ReadLine();
 
-            Console.Write("Age: ");
-            int Age = int.Parse(Console.ReadLine());
+            //Console.Write("Name: ");
+            //string Name = Console.ReadLine();
 
-            Console.WriteLine("salary: ");
-            float Salary = float.Parse(Console.ReadLine());
+            //Console.Write("Age: ");
+            //int Age = int.Parse(Console.ReadLine());
 
-
-            Console.WriteLine("hello " + Name + " welcome to spark");
-            Console.WriteLine("You are:  " + Age + " years old");
-            Console.WriteLine("You make " + Salary + " OMR");
-            ///////////////////////////////////////////////////////////////////////////////
-
-            Console.WriteLine("enter first number: ");
-            float A = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("enter second number: ");
-            float B  = float.Parse(Console.ReadLine());
+            //Console.WriteLine("salary: ");
+            //float Salary = float.Parse(Console.ReadLine());
 
 
-            float R1 = A + B;
-            float R2 = A - B;
-            float R3 = A * B;
-            float R4 = A / B;
-            float R5 = A % B;
+            //Console.WriteLine("hello " + Name + " welcome to spark");
+            //Console.WriteLine("You are:  " + Age + " years old");
+            //Console.WriteLine("You make " + Salary + " OMR");
 
-            Console.WriteLine("Addition result: " + R1);
-            Console.WriteLine("Subtraction result: " + R2);
-            Console.WriteLine("multiplication result: " + R3);
-            Console.WriteLine("Division result: " + R4);
-            Console.WriteLine("Reminder result: " + R5);
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+
+            //Console.WriteLine("enter first number: ");
+            //float A = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter second number: ");
+            //float B  = float.Parse(Console.ReadLine());
+
+
+            //float R1 = A + B;
+            //float R2 = A - B;
+            //float R3 = A * B;
+            //float R4 = A / B;
+            //float R5 = A % B;
+
+            //Console.WriteLine("Addition result: " + R1);
+            //Console.WriteLine("Subtraction result: " + R2);
+            //Console.WriteLine("multiplication result: " + R3);
+            //Console.WriteLine("Division result: " + R4);
+            //Console.WriteLine("Reminder result: " + R5);
+
+
+            /////////////////////////////////////////////////////////////////////////////////
             
 
+            Console.WriteLine("mark 1: ");
+            double mark1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("mark 2: ");
+            double mark2 = double.Parse(Console.ReadLine());
+            double result = (mark1 + mark2) / 2;
+            Console.WriteLine("result: " + result);
+            if (result < 50)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (result < 65 && result > 50)
+            {
+                Console.WriteLine("Pass");
+            }
+            else if (result < 80 && result > 65)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (result < 90 && result > 80)
+            {
+                Console.WriteLine("Very Good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
         }
-    }
+}
 }
