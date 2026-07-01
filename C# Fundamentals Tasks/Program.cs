985b69ea8be8be17ba5c65f5317b57a169deab74
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-01 09:09:59 +0400

    Completed Task 2

--- C# Fundamentals Tasks/Program.cs
+++ C# Fundamentals Tasks/Program.cs
@@ -8,14 +8,25 @@ namespace C__Fundamentals_Tasks
         {
             // Task 1:
 
-            String name = "Haitham";
-            int Age = 26;
-            double Height = 1.75;
-            bool is_student = false;
-            Console.WriteLine("Name: "+ name+", Age: "+ Age+", Height: "+ Height+", Student: "+ is_student);
+            //String name = "Haitham";
+            //int Age = 26;
+            //double Height = 1.75;
+            //bool is_student = false;
+            //Console.WriteLine("Name: "+ name+", Age: "+ Age+", Height: "+ Height+", Student: "+ is_student);
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////
-            
+
+            // Task 2:
+
+            Console.WriteLine("Enter Rectangle length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Rectangle width: ");
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            double Perimeter = 2* (length + width);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + Perimeter);
         }
     }
 }
