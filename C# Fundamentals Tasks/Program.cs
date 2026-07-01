075b974c06b57581db45bd7b904d5b523c926830
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-01 10:06:03 +0400

    Completed Task 11

--- C# Fundamentals Tasks/Program.cs
+++ C# Fundamentals Tasks/Program.cs
@@ -227,60 +227,107 @@ namespace C__Fundamentals_Tasks
 
             // Task 10:
 
-            Console.WriteLine("Enter First number: ");
-            double num1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Second number: ");
-            double num2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter an operator\n 1)+\n 2)-\n 3)*\n 4)/\n 5)%");
-            int option = int.Parse(Console.ReadLine());
-            switch (option)
+            //Console.WriteLine("Enter First number: ");
+            //double num1 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second number: ");
+            //double num2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter an operator\n 1)+\n 2)-\n 3)*\n 4)/\n 5)%");
+            //int option = int.Parse(Console.ReadLine());
+            //switch (option)
+            //{
+            //    case 1:
+            //        double resault = num1 + num2;
+            //        Console.WriteLine("The result of addition is: " + resault);
+            //        break;
+
+            //    case 2:
+            //        double resault2 = num1 - num2;
+            //        Console.WriteLine("The result of subtraction is: " + resault2);
+            //        break;
+
+            //    case 3:
+            //        double resault3 = num1 * num2;
+            //        Console.WriteLine("The result of multiplication is: " + resault3);
+            //        break;
+
+            //    case 4:
+            //        if (num2 != 0)
+            //        {
+            //            double resault4 = num1 / num2;
+            //            Console.WriteLine("The result of division is: " + resault4);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Cannot divide by zero");
+            //        }
+            //        break;
+
+            //    case 5:
+            //        if (num2 != 0)
+            //        {
+            //            double resault5 = num1 % num2;
+            //            Console.WriteLine("The result of modulus is: " + resault5);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Cannot divide by zero");
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid operator");
+            //        break;
+            //}
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Task 11:
+
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ebter your monthly income: ");
+            double income = double.Parse(Console.ReadLine());
+            Console.WriteLine("Do you have existing loan?: (yes/no) ");
+            string hasLoan = Console.ReadLine().ToLower();
+            bool canloan = false;
+            if (hasLoan == "yes")
             {
-                case 1:
-                    double resault = num1 + num2;
-                    Console.WriteLine("The result of addition is: " + resault);
-                    break;
-
-                case 2:
-                    double resault2 = num1 - num2;
-                    Console.WriteLine("The result of subtraction is: " + resault2);
-                    break;
-
-                case 3:
-                    double resault3 = num1 * num2;
-                    Console.WriteLine("The result of multiplication is: " + resault3);
-                    break;
-
-                case 4:
-                    if (num2 != 0)
-                    {
-                        double resault4 = num1 / num2;
-                        Console.WriteLine("The result of division is: " + resault4);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Cannot divide by zero");
-                    }
-                    break;
-
-                case 5:
-                    if (num2 != 0)
-                    {
-                        double resault5 = num1 % num2;
-                        Console.WriteLine("The result of modulus is: " + resault5);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Cannot divide by zero");
-                    }
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid operator");
-                    break;
-
-
+                canloan = false;
+            }else if (hasLoan == "no")
+            {
+                canloan = true;
             }
-    }
+
+            if (age >= 21 && age <= 60 && income >= 400 && canloan)
+            {
+                Console.WriteLine("You are eligible for the loan.");
+            }
+            else if (age < 21)
+            {
+                Console.WriteLine("You are not eligible for the loan Because you are Under 21.");
+            }
+            else if (age > 60)
+            {
+                Console.WriteLine("You are not eligible for the loan Because you are Over 60.");
+            }
+            else if (!canloan)
+            {
+                Console.WriteLine("You are not eligible for the loan Because you have an existing loan.");
+            }
+            else if (income < 400)
+            {
+                Console.WriteLine("You are not eligible for the loan Because your income is less than 400.");
+            }
+
+
+
+
+
+        }
+
+        }
 }
-}
+
+
+
 
