c3e636d3b627669e64ad60f1a034d024a9ea6fcc
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-01 09:06:15 +0400

    completed Task 1

---
+++ C# Fundamentals Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata;
+
+namespace C__Fundamentals_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1:
+
+            String name = "Haitham";
+            int Age = 26;
+            double Height = 1.75;
+            bool is_student = false;
+            Console.WriteLine("Name: "+ name+", Age: "+ Age+", Height: "+ Height+", Student: "+ is_student);
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+        }
+    }
+}
