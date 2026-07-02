b7ed45ba17e214e3d8c67b522a1e0c19b564f911
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-02 08:15:33 +0400

    Add project files

---
+++ C# Fundamentals Tasks 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Fundamentals_Tasks_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
