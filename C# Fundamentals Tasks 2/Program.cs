418db524d900c1c687726b3988cb0f0aca5545f3
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-02 09:57:13 +0400

    Completed problem 10

--- C# Fundamentals Tasks 2/Program.cs
+++ C# Fundamentals Tasks 2/Program.cs
@@ -182,40 +182,155 @@ namespace C__Fundamentals_Tasks_2
 
             // Problem 9:
 
+            //bool is_running = true;
+            //int sum = 0;
+            //int num1 = 0;
+            //do
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("please enter a positive whole number: ");
+            //        num1 = int.Parse(Console.ReadLine());
+            //        if (num1 <= 0)
+            //        {
+            //            Console.WriteLine("Invalid input, please enter a positive whole number: ");
+            //        }
+            //        else
+            //        {
+            //            is_running= false;
+            //        }
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Error: Invalid input. Please enter a valid number.");
+            //    }
+
+
+            //} while (is_running);
+
+            //for (int i = 1; i <= num1; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine("The sum from 1 to " + num1 + " is: " + sum);
+
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Problem 9:
+
+            int pin = 1234;
+            double startingBalance = 100.000;
             bool is_running = true;
-            int sum = 0;
-            int num1 = 0;
-            do
+            int gusses = 0;
+            bool bank = false;
+            while (is_running)
             {
                 try
                 {
-                    Console.WriteLine("please enter a positive whole number: ");
-                    num1 = int.Parse(Console.ReadLine());
-                    if (num1 <= 0)
+                    Console.WriteLine("Please enter your pin: ");
+                    int userPin = int.Parse(Console.ReadLine());
+                    if (userPin != pin)
                     {
-                        Console.WriteLine("Invalid input, please enter a positive whole number: ");
-                    }
-                    else
+                        gusses++;
+                        Console.WriteLine("Incorrect pin, please try again.");
+                        if (gusses == 3)
+                        {
+                            Console.WriteLine("You have entered the wrong pin 3 times, Card blocked.");
+                            is_running = false;
+
+                        }
+                    }else
                     {
-                        is_running= false;
+                        bank = true;
                     }
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("Error: Invalid input. Please enter a valid number.");
                 }
+
+
+                while (bank == true)
+                {
+                    try
+                    {
+                        Console.WriteLine("******************************************");
+                        Console.WriteLine("welcome to Bank:");
+                        Console.WriteLine("1.Withdraw");
+                        Console.WriteLine("2.Deposite");
+                        Console.WriteLine("3.Balance check");
+                        Console.WriteLine("4.Quit");
+                        Console.WriteLine("choose an option: ");
+                        int option = int.Parse(Console.ReadLine());
+                        Console.WriteLine("******************************************");
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("your balance = " + startingBalance);
+                                Console.WriteLine("enter amount to withdraw: ");
+                                double withdraw = double.Parse(Console.ReadLine());
+                                if (withdraw > startingBalance)
+                                {
+                                    Console.WriteLine("Insufficient funds.");
+                                }
+                                else if(withdraw <= 0)
+                                {
+                                    Console.WriteLine("Invalid withdraw amount. Please enter a positive value.");
+                                }
+                                else
+                                {
+                                    startingBalance -= withdraw;
+                                    Console.WriteLine("your new balance = " + startingBalance);
+                                    Console.WriteLine("your withdrawed amount = " + withdraw);
+                                }
+                                break;
+
+                             case 2:
+                                Console.WriteLine("your balance =" + startingBalance);
+                                Console.WriteLine("enter amount to deposite: ");
+                                double deposite = double.Parse(Console.ReadLine());
+                                if (deposite <= 0)
+                                {
+                                    Console.WriteLine("Invalid deposit amount. Please enter a positive value.");
+                                }
+                                else
+                                {
+                                    startingBalance += deposite;
+                                    Console.WriteLine("your new balance = " + startingBalance);
+                                }
+                                
+                                Console.WriteLine("your new balance = " + startingBalance);
+                                break;
+
+                            case 3:
+                                Console.WriteLine("your balance = " + startingBalance);
+                                break;
+
+                            case 4:
+                                bank = false;
+                                is_running = false;
+                                break;
+
+
+                        }
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error: Invalid input. Please enter a valid number.");
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+                    
                 
 
-            } while (is_running);
-
-            for (int i = 1; i <= num1; i++)
-            {
-                sum += i;
-            }
-            Console.WriteLine("The sum from 1 to " + num1 + " is: " + sum);
-        }
-}
-}
 
 
 
