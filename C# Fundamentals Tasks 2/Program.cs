fdd5e047d62b3ec29a3963b633a8b4ae1a3183ac
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-02 08:49:22 +0400

    Completed problem 3

--- C# Fundamentals Tasks 2/Program.cs
+++ C# Fundamentals Tasks 2/Program.cs
@@ -21,20 +21,35 @@ namespace C__Fundamentals_Tasks_2
 
             // Problem 2:
 
-            Console.Write("Enter a positive whole number: ");
+            //Console.Write("Enter a positive whole number: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= num1; i++) 
+            //{
+
+            //    sum += i;
+
+            //}
+            //Console.WriteLine("The sum from 1 to " + num1 + " is: " + sum);
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Problem 3:
+
+            Console.Write("Enter a number: ");
             int num1 = int.Parse(Console.ReadLine());
             int sum = 0;
-            for (int i = 1; i <= num1; i++) 
-            {
-                
-                sum += i;
-                
+            int i = 0;
+            for ( i=0; i <= 10; i++)
+            { 
+                Console.WriteLine(num1 + "X" + i + "=" + (num1 * i));
             }
-            Console.WriteLine("The sum from 1 to " + num1 + " is: " + sum);
+        }
         }
 
 
     }
-    }
+   
     
 
