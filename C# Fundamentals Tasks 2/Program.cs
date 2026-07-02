b9c8c3bb71cff2376506e4446b817d9fa1a6940c
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-02 08:26:36 +0400

    Completed problem 1

--- C# Fundamentals Tasks 2/Program.cs
+++ C# Fundamentals Tasks 2/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace C__Fundamentals_Tasks_2
+﻿using System.Runtime.InteropServices;
+
+namespace C__Fundamentals_Tasks_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            for (int i = num1; i >= 1; i--) 
+            { 
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
         }
     }
-}
+    }
+
