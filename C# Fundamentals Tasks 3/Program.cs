33b825afa9501d4da22d8b15c8120f470da2771b
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-05 12:08:28 +0400

    Completed problem 1

--- C# Fundamentals Tasks 3/Program.cs
+++ C# Fundamentals Tasks 3/Program.cs
@@ -5,7 +5,25 @@
         static void Main(string[] args)
         {
             // Task 1:
-
-        }
+            bool is_running = true;
+            while (is_running)
+            {
+                try
+                {
+                    Console.WriteLine("Enter First Number: ");
+                    double FirstNumber = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Second Number: ");
+                    double SecondNumber = double.Parse(Console.ReadLine());
+                    double resault = SecondNumber - FirstNumber;
+                    double absoluteResault = Math.Abs(resault);
+                    Console.WriteLine("The absolute value of the difference is: " + absoluteResault);
+                    is_running = false;
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
     }
+}
+}
 }
