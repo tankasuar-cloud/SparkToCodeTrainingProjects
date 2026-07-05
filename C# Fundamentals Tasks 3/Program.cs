2d13aecec80b646ea87e1503428b3291cae647f8
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-05 12:14:04 +0400

    Completed problem 2

--- C# Fundamentals Tasks 3/Program.cs
+++ C# Fundamentals Tasks 3/Program.cs
@@ -4,26 +4,49 @@
     {
         static void Main(string[] args)
         {
-            // Task 1:
+            // problem 1:
+            //        bool is_running = true;
+            //        while (is_running)
+            //        {
+            //            try
+            //            {
+            //                Console.WriteLine("Enter First Number: ");
+            //                double FirstNumber = double.Parse(Console.ReadLine());
+            //                Console.WriteLine("Enter Second Number: ");
+            //                double SecondNumber = double.Parse(Console.ReadLine());
+            //                double resault = SecondNumber - FirstNumber;
+            //                double absoluteResault = Math.Abs(resault);
+            //                Console.WriteLine("The absolute value of the difference is: " + absoluteResault);
+            //                is_running = false;
+            //            }
+            //            catch(FormatException)
+            //            {
+            //                Console.WriteLine("Invalid input. Please enter a valid number.");
+            //            }
+            //}
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///
+            //problem 2:
+
             bool is_running = true;
             while (is_running)
             {
                 try
                 {
-                    Console.WriteLine("Enter First Number: ");
-                    double FirstNumber = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter Second Number: ");
-                    double SecondNumber = double.Parse(Console.ReadLine());
-                    double resault = SecondNumber - FirstNumber;
-                    double absoluteResault = Math.Abs(resault);
-                    Console.WriteLine("The absolute value of the difference is: " + absoluteResault);
+                    Console.WriteLine("Enter A Number: ");
+                    double Number = double.Parse(Console.ReadLine());
+                    double squareRoot = Math.Sqrt(Number);
+                    double pwr = Math.Pow(Number, 2);
+                    Console.WriteLine($"Square root = {squareRoot}, Square = {pwr}");
                     is_running = false;
                 }
-                catch(FormatException)
+                catch (FormatException)
                 {
                     Console.WriteLine("Invalid input. Please enter a valid number.");
                 }
-    }
+            }
 }
 }
 }
