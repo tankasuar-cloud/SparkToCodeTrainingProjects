adeb4b6d8448ca4dc26d6c418266c4834ed42994
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-06 15:57:20 +0400

    Completed problem 1

--- C# fundamentals Task 4/Program.cs
+++ C# fundamentals Task 4/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        public static void PrintWelcome(string name)
+        { 
+            Console.WriteLine($"Welcome {name}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            PrintWelcome(name);
+        }
         }
     }
-}
+
