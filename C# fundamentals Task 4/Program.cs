718a638b91e58e5fc9cafeb3e6f11fad5516963f
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-06 16:05:57 +0400

    Completed problem 2

--- C# fundamentals Task 4/Program.cs
+++ C# fundamentals Task 4/Program.cs
@@ -1,17 +1,50 @@
-﻿namespace C__fundamentals_Task_4
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C__fundamentals_Task_4
 {
     internal class Program
     {
-        public static void PrintWelcome(string name)
-        { 
-            Console.WriteLine($"Welcome {name}");
+        //problem 1:
+        //public static void PrintWelcome(string name)
+        //{ 
+        //    Console.WriteLine($"Welcome {name}");
+        //}
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //problem 2:
+
+        public static int Square(int number)
+        {
+            int result = number * number;
+            return result;
+
         }
+
+
         static void Main(string[] args)
         {
-            Console.Write("What is your name? ");
-            string name = Console.ReadLine();
-            PrintWelcome(name);
-        }
+
+            //problem 1:
+
+            //Console.Write("What is your name? ");
+            //string name = Console.ReadLine();
+            //PrintWelcome(name);
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            //problem 2:
+
+            Console.Write("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Square root of {number} is: {Square(number)}");
+
         }
     }
+
+}
+
 
