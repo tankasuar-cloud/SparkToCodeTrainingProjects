35eb001435dee323df738ec72796365c6ccdff95
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-07 13:58:06 +0400

    Fixed problem 1

--- C# fundamentals Task 5/Program.cs
+++ C# fundamentals Task 5/Program.cs
@@ -4,16 +4,29 @@
     {
         static void Main(string[] args)
         {
-            int[] grades = new int[5];
-            for (int i = 0; i < grades.Length; i++)
+            
+            bool is_running = true;
+            while (is_running)
             {
-                Console.Write($"please enter a grade for Index {i}: ");
-                int num = int.Parse(Console.ReadLine());
-                grades[i] = num;
-            }
-            foreach (int grade in grades)
-            {
-                Console.Write(grade+ " ");
+                try
+                {
+                    int[] grades = new int[5];
+                    for (int i = 0; i < grades.Length; i++)
+                    {
+                        Console.Write($"please enter a grade for Index {i}: ");
+                        int num = int.Parse(Console.ReadLine());
+                        grades[i] = num;
+                    }
+                    foreach (int grade in grades)
+                    {
+                        Console.Write(grade + " ");
+                    }
+                    is_running = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
             }
         }
     }
