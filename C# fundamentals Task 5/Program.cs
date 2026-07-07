de9cb93af440e81eee8bd646bc786ae58c07a056
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-07 14:04:44 +0400

    Completed problem 2

--- C# fundamentals Task 5/Program.cs
+++ C# fundamentals Task 5/Program.cs
@@ -4,30 +4,59 @@
     {
         static void Main(string[] args)
         {
+            // problem 1:
+
+
+            //bool is_running = true;
+            //while (is_running)
+            //{
+            //    try
+            //    {
+            //        int[] grades = new int[5];
+            //        for (int i = 0; i < grades.Length; i++)
+            //        {
+            //            Console.Write($"please enter a grade for Index {i}: ");
+            //            int num = int.Parse(Console.ReadLine());
+            //            grades[i] = num;
+            //        }
+            //        foreach (int grade in grades)
+            //        {
+            //            Console.Write(grade + " ");
+            //        }
+            //        is_running = false;
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid number.");
+            //    }
+            //}
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+            // problem 2:
+
+
+            List<string> todo = new List<string>();
             
-            bool is_running = true;
-            while (is_running)
-            {
-                try
-                {
-                    int[] grades = new int[5];
-                    for (int i = 0; i < grades.Length; i++)
-                    {
-                        Console.Write($"please enter a grade for Index {i}: ");
-                        int num = int.Parse(Console.ReadLine());
-                        grades[i] = num;
-                    }
-                    foreach (int grade in grades)
-                    {
-                        Console.Write(grade + " ");
-                    }
-                    is_running = false;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
+                
+            for (int i = 0; i < 5; i++)
+            { 
+                Console.WriteLine("Add a task to to-do list: ");
+                string task = Console.ReadLine();
+                todo.Add(task);
             }
+            int taskNumber = 1;
+            Console.WriteLine("\n--- Your To-Do List ---");
+            foreach (string task in todo)
+            {
+                Console.WriteLine($"{taskNumber}. {task}");
+                taskNumber++;
+            }
+                    
+                
+                
+            
+
+
         }
     }
 }
