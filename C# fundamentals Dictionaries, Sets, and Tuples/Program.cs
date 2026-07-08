a9dfcccc4db29e974e154aab915d37fd9540d14f
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-08 13:53:02 +0400

    Completed problem 3

--- C# fundamentals Dictionaries, Sets, and Tuples/Program.cs
+++ C# fundamentals Dictionaries, Sets, and Tuples/Program.cs
@@ -40,27 +40,94 @@ namespace C__fundamentals_Dictionaries__Sets__and_Tuples
             //}
 
 
-        //Task 2 - Unique Guest List
-        //Create a HashSet<string> to manage a guest list for a party.
-        //Use a loop to ask the user to enter 5 guest names.Print the final count of unique guests and then list out all the names.
-        // Requirements:
-        // Use.Add to insert the names into the HashSet<string>.
-        // Notice and verify how entering the same name twice does not increase the count.
+            //Task 2 - Unique Guest List
+            //Create a HashSet<string> to manage a guest list for a party.
+            //Use a loop to ask the user to enter 5 guest names.Print the final count of unique guests and then list out all the names.
+            // Requirements:
+            // Use.Add to insert the names into the HashSet<string>.
+            // Notice and verify how entering the same name twice does not increase the count.
 
-            HashSet<string> guestList = new HashSet<string>();
-            for (int i = 0; i < 5; i++)
+            //HashSet<string> guestList = new HashSet<string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"Please enter quest {i + 1} Name: ");
+            //    string name = Console.ReadLine();
+            //    guestList.Add(name);
+
+            //}
+            //Console.WriteLine($"\nTotal unique guests: {guestList.Count}");
+            //foreach (string guest in guestList)
+            //{
+            //    Console.WriteLine($"- {guest}");
+            //}
+
+
+
+            //Task 3 - Inventory Price Scanner
+            //Create a Dictionary<string, double> to store item names and their prices.
+            //Use a while loop to let a store manager keep entering items and prices until they type "stop".
+            //Afterward, ask the user to type an item name to check its price.
+            //Requirements:
+            //Use a loop that saves items into the dictionary using user input.
+            //Print the full inventory list with names and prices before asking the user to search.
+
+
+            Dictionary<string, double> inventory = new Dictionary<string, double>();
+            bool is_running = true;
+            while (is_running)
             {
-                Console.Write($"Please enter quest {i + 1} Name: ");
-                string name = Console.ReadLine();     
-                guestList.Add(name);
                 
-            }
-            Console.WriteLine($"\nTotal unique guests: {guestList.Count}");
-            foreach (string guest in guestList)
+                    Console.Write("Please enter item name (or type 'exit' to finish): ");
+                    string name = Console.ReadLine();
+                    if (name.ToLower() == "exit")
+                    {
+                        is_running = false;
+                        continue;
+                    }
+                try
+                {
+                    Console.Write($"Please enter a price for {name}: ");
+                    double price = double.Parse(Console.ReadLine());
+                    if (!inventory.ContainsKey(name))
+                    {
+                        inventory.Add(name, price);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item already exists in inventory.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter a valid Price");
+                }
+                }
+            is_running = true;
+            Console.WriteLine("\n--- Current Inventory ---");
+            foreach (var item in inventory)
             {
-                Console.WriteLine($"- {guest}");
+                Console.WriteLine(item.Key);
             }
-
+            Console.WriteLine("------------------------");
+            while (is_running)
+            {
+                Console.Write("Enter an item name to check its price (or type 'exit' to quit): ");
+                string searchName = Console.ReadLine();
+                if (searchName.ToLower() == "exit")
+                {
+                    is_running = false;
+                }
+                else if (inventory.ContainsKey(searchName))
+                {
+                    Console.WriteLine($"Price: {inventory[searchName]}");
+                    Console.WriteLine("------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("Item not found in inventory.\n");
+                    Console.WriteLine("------------------------");
+                }
+            }
         }
     }
 }
