e02622486f46a54c238774214a44c8471a4db330
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-08 13:33:03 +0400

    Completed problem 2

--- C# fundamentals Dictionaries, Sets, and Tuples/Program.cs
+++ C# fundamentals Dictionaries, Sets, and Tuples/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Runtime.Intrinsics.X86;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace C__fundamentals_Dictionaries__Sets__and_Tuples
@@ -17,25 +18,47 @@ namespace C__fundamentals_Dictionaries__Sets__and_Tuples
             //Initialize the dictionary with 3 hardcoded entries.
             //Use ContainsKey to check if the name exists before printing. If it does not exist, print a friendly error message.
 
-            Dictionary<string, string> Phonebook = new Dictionary<string, string>();
-            Phonebook.Add("Ahmed", "99112288");
-            Phonebook.Add("Ali", "93344214");
-            Phonebook.Add("Khalid", "95674382");
-            foreach (var phone in Phonebook)
-            {
-                Console.WriteLine($"Name: {phone.Key}");
+            //Dictionary<string, string> Phonebook = new Dictionary<string, string>();
+            //Phonebook.Add("Ahmed", "99112288");
+            //Phonebook.Add("Ali", "93344214");
+            //Phonebook.Add("Khalid", "95674382");
+            //foreach (var phone in Phonebook)
+            //{
+            //    Console.WriteLine($"Name: {phone.Key}");
 
-            }
-            Console.Write("Enter a name to search: ");
-            string searchName = Console.ReadLine();
+            //}
+            //Console.Write("Enter a name to search: ");
+            //string searchName = Console.ReadLine();
 
-            if (Phonebook.ContainsKey(searchName))
+            //if (Phonebook.ContainsKey(searchName))
+            //{
+            //    Console.WriteLine($"Phone number: {Phonebook[searchName]}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, that name is not in the phonebook.");
+            //}
+
+
+        //Task 2 - Unique Guest List
+        //Create a HashSet<string> to manage a guest list for a party.
+        //Use a loop to ask the user to enter 5 guest names.Print the final count of unique guests and then list out all the names.
+        // Requirements:
+        // Use.Add to insert the names into the HashSet<string>.
+        // Notice and verify how entering the same name twice does not increase the count.
+
+            HashSet<string> guestList = new HashSet<string>();
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"Phone number: {Phonebook[searchName]}");
+                Console.Write($"Please enter quest {i + 1} Name: ");
+                string name = Console.ReadLine();     
+                guestList.Add(name);
+                
             }
-            else
+            Console.WriteLine($"\nTotal unique guests: {guestList.Count}");
+            foreach (string guest in guestList)
             {
-                Console.WriteLine("Sorry, that name is not in the phonebook.");
+                Console.WriteLine($"- {guest}");
             }
 
         }
