b885339187204bf00bfa72ac7776eda69fceda9a
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-09 09:43:38 +0400

    Fixed an error in show all accounts function

--- BankingSystemApp/Program.cs
+++ BankingSystemApp/Program.cs
@@ -21,7 +21,7 @@ namespace BankingSystemApp
                 Console.WriteLine("3. Withdraw Money");
                 Console.WriteLine("4. Show Balance");
                 Console.WriteLine("5. Transfer Amount");
-                Console.WriteLine("6. Show All Accounts);
+                Console.WriteLine("6. Show All Accounts");
                 Console.WriteLine("7. <your 2nd custom service - choose a name>");
                 Console.WriteLine("8. Exit");
                 Console.Write("Choose an option: ");
