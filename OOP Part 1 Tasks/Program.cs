8608db6fafc74dec7bdffdb9e61e6fc2eab97120
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-12 08:36:56 +0400

    Added Project files

---
+++ OOP Part 1 Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Part_1_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
