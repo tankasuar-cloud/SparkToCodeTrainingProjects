bbf123f6b9108418f5fe7b2ee55b55c25135675a
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-14 10:06:49 +0400

    Completed the classes and the menu

--- TS-OOP-01/HotelManagementSystem/Program.cs
+++ TS-OOP-01/HotelManagementSystem/Program.cs
@@ -2,10 +2,13 @@
 {
     internal class Program
     {
+        static List<Room> rooms = new List<Room>();
+        static List<Guest> guests = new List<Guest>();
         static void Main(string[] args)
         {
-            bool running = true;
-            while (running)
+            PreloadRooms();
+            bool is_running = true;
+            while (is_running)
             {
                 Console.Clear();
                 Console.WriteLine("=== HOTEL MANAGEMENT SYSTEM ===");
@@ -26,9 +29,30 @@
                 Console.WriteLine("15. Guest Pagination Viewer");
                 Console.WriteLine("0. Exit");
                 Console.Write("\nEnter your choice: ");
+                int option;
+                try { option = int.Parse(Console.ReadLine());}
+                catch (FormatException) { Console.WriteLine("Invalid option.");continue; }
+                switch (option)
+                {
+                    case 0: is_running = false; break;
+
+                }
+                Console.WriteLine("press any key.");
+                Console.ReadKey();
+                Console.Clear();
 
 
             }
         }
+        static void PreloadRooms()
+        {
+            rooms.Add(new Room(101, "Single", 25.00));
+            rooms.Add(new Room(102, "Single", 25.00));
+            rooms.Add(new Room(201, "Double", 40.00));
+            rooms.Add(new Room(202, "Double", 40.00));
+            rooms.Add(new Room(301, "Suite", 75.00));
+            rooms.Add(new Room(302, "Suite", 75.00));
+        }
+        
     }
 }
--- TS-OOP-01/HotelManagementSystem/Room.cs
+++ TS-OOP-01/HotelManagementSystem/Room.cs
@@ -6,5 +6,21 @@ namespace HotelManagementSystem
 {
     internal class Room
     {
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public double PricePerNight { get; set; }
+        public bool IsAvailable { get; set; }
+        public Room(int roomNumber, string roomType, double pricePerNight)
+        {
+            RoomNumber = roomNumber;
+            RoomType = roomType;
+            PricePerNight = pricePerNight;
+            IsAvailable = true;
+        }
+        public void DisplayRoom()
+        {
+            string status = IsAvailable ? "Available" : "Booked";
+            Console.WriteLine($"Room: {RoomNumber} | Type: {RoomType} | Price: OMR {PricePerNight} | Status: {status}");
+        }
     }
 }
