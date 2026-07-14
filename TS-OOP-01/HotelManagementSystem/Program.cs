418c89fce1258372603d3beccb6ef40f16dccfc4
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-14 10:29:11 +0400

    Completed Case 1

--- TS-OOP-01/HotelManagementSystem/Program.cs
+++ TS-OOP-01/HotelManagementSystem/Program.cs
@@ -10,8 +10,9 @@
             bool is_running = true;
             while (is_running)
             {
-                Console.Clear();
-                Console.WriteLine("=== HOTEL MANAGEMENT SYSTEM ===");
+                Console.WriteLine("================================================");
+                Console.WriteLine("=== GRAND VISTA HOTEL — MANAGEMENT SYSTEM ===");
+                Console.WriteLine("================================================");
                 Console.WriteLine("1. Add New Room");
                 Console.WriteLine("2. Register New Guest");
                 Console.WriteLine("3. Book a Room for a Guest");
@@ -28,19 +29,22 @@
                 Console.WriteLine("14. Highest Revenue Booking");
                 Console.WriteLine("15. Guest Pagination Viewer");
                 Console.WriteLine("0. Exit");
+                Console.WriteLine("================================================");
                 Console.Write("\nEnter your choice: ");
                 int option;
-                try { option = int.Parse(Console.ReadLine());}
+                try { option = int.Parse(Console.ReadLine()??"");}
                 catch (FormatException) { Console.WriteLine("Invalid option.");continue; }
                 switch (option)
                 {
                     case 0: is_running = false; break;
+                    case 1: AddNewRoom(); break;
+
 
                 }
                 Console.WriteLine("press any key.");
                 Console.ReadKey();
                 Console.Clear();
-
+                
 
             }
         }
@@ -53,6 +57,35 @@
             rooms.Add(new Room(301, "Suite", 75.00));
             rooms.Add(new Room(302, "Suite", 75.00));
         }
-        
+        static void AddNewRoom()
+        {
+            Console.WriteLine("================================================");
+            Console.Write("Please enter Room number: ");
+            int num;
+            try { num = int.Parse(Console.ReadLine()??""); }
+            catch (FormatException) { Console.WriteLine("Invalid number."); return; }
+            if (rooms.Any(r => r.RoomNumber == num)) {Console.WriteLine("Room already exists.");return;}
+            Console.WriteLine("================================================");
+            Console.Write("Please enter room type (Single / Double / Suite): ");
+            string roomType = Console.ReadLine() ?? "";
+            if (roomType != "Single" && roomType != "Double" && roomType != "Suite")
+            { Console.WriteLine("Wrong Room Type.");return; }
+
+            Console.WriteLine("================================================");
+            Console.Write("Please enter Room Price per night: ");
+            double price;
+            try { price = double.Parse(Console.ReadLine() ?? ""); }
+            catch (FormatException) { Console.WriteLine("Invalid number."); return; }
+            rooms.Add(new Room(num, roomType, price));
+            Console.WriteLine("================================================");
+            Console.WriteLine("\n--- Room Added Successfully! ---");
+            Console.WriteLine($"Room Number: {num}");
+            Console.WriteLine($"Room Type:   {roomType}");
+            Console.WriteLine($"Price/Night:  {price} OMR");
+            Console.WriteLine($"Total Rooms: {rooms.Count}");
+            Console.WriteLine("================================================");
+        }
+
+
     }
 }
