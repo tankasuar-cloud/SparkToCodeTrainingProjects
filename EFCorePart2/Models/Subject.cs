0fd3f7717dd4a7c768cb522445129c7aba214dd6
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-26 09:49:12 +0400

    Completed Subject class

---
+++ EFCorePart2/Models/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFCorePart2.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
