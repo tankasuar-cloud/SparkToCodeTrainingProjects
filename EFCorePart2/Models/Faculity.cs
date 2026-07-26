84037cc3efc131d27fb6d073009355e807356d87
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-26 09:45:39 +0400

    Completed Faculity Class

---
+++ EFCorePart2/Models/Faculity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFCorePart2.Models
+{
+    public class Faculity
+    {
+        [Key]
+        public int F_id { get; set; }
+        public String Name { get; set; }
+        public int Mobile_no { get; set; }
+        public double Salary { get; set; }
+
+
+    }
+}
