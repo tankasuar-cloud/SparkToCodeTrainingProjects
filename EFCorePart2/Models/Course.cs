ce52dc40ad6b9b75956340b920fa3e90d3dad024
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-26 09:56:33 +0400

    Completed Course Class

---
+++ EFCorePart2/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCorePart2.Models
+{
+    internal class Course
+    {
+        [Key]
+        public int Course_id { get; set; }
+        public string course_name { get; set; }
+        public  int duration { get; set; }
+
+    }
+}
