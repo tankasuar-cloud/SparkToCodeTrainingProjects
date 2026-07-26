a6022cea8b30fc18ecefb377c84af8099d7af1c1
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-26 09:47:28 +0400

    Completed Department Class

---
+++ EFCorePart2/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFCorePart2.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+}
