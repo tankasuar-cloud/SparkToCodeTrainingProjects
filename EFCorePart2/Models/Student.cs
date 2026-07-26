e3dbb1ae2f9b8c487ac2f587c7aa256ec296a853
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-26 09:54:40 +0400

    Completed Student Class

---
+++ EFCorePart2/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCorePart2.Models
+{
+    public class Student
+    {
+        [Key]
+        public int S_id { get; set; }
+        public string F_name { get; set; }
+        public string L_name { get; set; }
+        public DateTime DOB { get; set; }
+        public int phone_number { get; set; }
+
+    }
+}
