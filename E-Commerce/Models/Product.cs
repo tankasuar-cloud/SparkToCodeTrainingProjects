de077001e2b7b889d559ab277471973cd314c7b9
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-27 10:06:37 +0400

    completed Product class

---
+++ E-Commerce/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
