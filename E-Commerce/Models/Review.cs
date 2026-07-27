8518b487a347199f81f985258802cec6b5afc796
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-27 10:09:28 +0400

    Completed Reviewclass

---
+++ E-Commerce/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
