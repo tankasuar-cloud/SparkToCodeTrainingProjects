5f5ca13bc8549be44af4bb43ffbe0f34eb76cbb1
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-27 10:05:32 +0400

    Completed Category class

--- E-Commerce/DbContext.cs
+++ E-Commerce/DbContext.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace E_Commerce
 {
-    internal class DbContext
+    public class DbContext
     {
+
+
     }
 }
---
+++ E-Commerce/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
