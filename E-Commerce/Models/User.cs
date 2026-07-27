e72ad4751353040c5beabc37ef427943970ce1fa
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-27 10:04:04 +0400

    Completed User Class

---
+++ E-Commerce/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+
+    }
+}
