a35f14c0ec96100629e0fd8c0df742e3bbd61809
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-27 10:08:09 +0400

    Completed Order class

---
+++ E-Commerce/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace E_Commerce.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
