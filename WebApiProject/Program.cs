3d65d3dbf59871663479e927bb79689faf9d6ddd
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-29 11:36:26 +0400

    Created Project Files

---
+++ WebApiProject/Program.cs
@@ -0,0 +1,31 @@
+
+namespace WebApiProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
