42213b49612d502e5c870b4f0cd81993796416b0
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-29 12:30:28 +0400

    Add new methods to ProductController for query and update

--- WebApiProject/Controllers/ProductController.cs
+++ WebApiProject/Controllers/ProductController.cs
@@ -40,6 +40,26 @@ namespace WebApiProject.Controllers
             return products;
 
         }
+        public List<Product> GetByname(string name)
+        {
+            List<Product> products = context.Product.Where(p=>p.ProductName.Contains( name)).ToList();
+            return products;
+        }
+
+        public void UpdateProductPrice(int id,decimal newPrice)
+        {
+            Product productt = context.Product.FirstOrDefault(p => p.ProductId == id);
+            productt.Price = newPrice;
+            context.SaveChanges();
+        }
+        public void UpdateProductname(int id, string name)
+        {
+            Product productt = context.Product.FirstOrDefault(p => p.ProductId == id);
+            productt.ProductName = name;
+            context.SaveChanges();
+        }
+
+
 
     }
 }
