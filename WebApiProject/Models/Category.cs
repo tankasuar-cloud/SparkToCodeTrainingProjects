21ff4fde702b947e0c652ee7749bb956518fe6dc
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-29 11:45:05 +0400

    added relationships to classes

--- WebApiProject/Models/Category.cs
+++ WebApiProject/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApiProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiProject.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
--- WebApiProject/Models/Product.cs
+++ WebApiProject/Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApiProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiProject.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } 
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+
+        [ForeignKey("_Category")]
+        public int CategoryId { get; set; }
+        public Category _Category { get; set; } 
     }
 }
+    
