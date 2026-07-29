7c61cec14158e628c71c16041b655f148f5c9646
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-29 13:37:44 +0400

    Added User and Order Classes

--- WebApiProject/Projectcontext.cs
+++ WebApiProject/Projectcontext.cs
@@ -6,6 +6,9 @@ namespace WebApiProject
     {
         public DbSet<Product> Product {  get; set; }
         public DbSet<Category> Category { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Order> Order { get; set; }
+
 
         public Projectcontext(DbContextOptions<Projectcontext> options) : base(options)
         {
