620f2525c1ac6e080859588b9ad5e58e8cb2aa73
Haitham Albusaidi <Tankasuar@gmail.com>
2026-07-29 12:19:29 +0400

    Enable DI and CRUD methods in controllers

--- WebApiProject/Controllers/CategoryController.cs
+++ WebApiProject/Controllers/CategoryController.cs
@@ -1,6 +1,31 @@
-﻿namespace WebApiProject.Controllers
+﻿using WebApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+namespace WebApiProject.Controllers
 {
     public class CategoryController
     {
+        private Projectcontext context;
+        public CategoryController(Projectcontext context)
+        {
+            context = context;
+        }
+        public void addCategory(Category category)
+        {
+            context.Category.Add(category);
+            context.SaveChanges();
+        }
+        public void DeleteProduct(int id)
+        {
+            Category category = context.Category.FirstOrDefault(p => p.CategoryId == id);
+            if (category == null)
+            {
+
+            }
+            else
+            {
+                context.Category.Remove(category);
+                context.SaveChanges();
+            }
+        }
     }
 }
--- WebApiProject/Controllers/ProductController.cs
+++ WebApiProject/Controllers/ProductController.cs
@@ -1,6 +1,35 @@
-﻿namespace WebApiProject.Controllers
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiProject.Models;
+namespace WebApiProject.Controllers
 {
     public class ProductController
     {
+        
+        private Projectcontext context;
+        public ProductController(Projectcontext context)
+        {
+            context = context;
+        }
+        public void AddProduct(Product p)
+        {
+            context.Product.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+           Product productt= context.Product.FirstOrDefault(p => p.ProductId == id);
+            if (productt == null )
+            {
+
+            }
+            else
+            {
+                context.Product.Remove(productt);
+                context.SaveChanges();
+            }
+        }
+
+
     }
 }
